eec52204703d7c2995e7162207a767341e3deb2d
MuteeVincente <vinniemutee@gmail.com>
2023-07-04 09:19:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+// App.js
+import Joke from "./Joke";
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			
+			<h3>Joke Generator Using React and Joke API</h3>
+			<Joke/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,12 @@
+// Button.js
+
+import React from "react";
+import './Button.css'
+
+const Button = (props) => {
+	return <button onClick={props.callApi}>
+		Click to generate a joke.
+	</button>;
+}
+// Export Button Component
+export default Button;
